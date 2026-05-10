9a981fc2488e032876edf7065fe032acb20d820c
OpenTech <open_tech_@outlook.com>
2026-05-10 17:13:11 +0100

    Update GitLab icon path in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ export const Footer: React.FC = () => {
             <a href="https://gitlab.com/users/Open-Tech-Project/projects" target="_blank" rel="noreferrer" className="text-slate-400 hover:text-white transition-colors" aria-label="GitLab">
               <span className="sr-only">GitLab</span>
               <svg viewBox="0 0 24 24" className="h-5 w-5 fill-current">
-                <path d="M12 23.999l-4.208-12.948H1.629L12 .977l10.371 10.074h-6.163L12 23.999z"/>
+                <path d="M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0 1-.3-.94l1.22-3.74 2.44-7.51A.42.42 0 0 1 4.82 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.51L23 13.45a.84.84 0 0 1-.35.94z"/>
               </svg>
             </a>
           </div>
